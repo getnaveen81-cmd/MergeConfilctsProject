8eea1fc74dd56944e83eeb920deed3ae6705edc2
naveen kumar s <getnaveen81@gmail.com>
2025-10-18 17:56:05 +0530

    4 commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
     }
 
     public static void printSomething(){
-        System.out.println("Print something");
+        System.out.println("Priint something");
         System.out.println("do");
     }
 
